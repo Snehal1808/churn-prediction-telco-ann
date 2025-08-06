d08753fa6d73de1d615c4699399ee678b8472ddd
Snehal1808 <snehalsubu18@gmail.com>
2025-08-06 14:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="SignalStay", layout="wide")
 
 st.markdown("""
     <h1 style='text-align: center; color: #1E3A8A;'>Signal<span style='color:#14B8A6;'>Lens</span></h1>
-    <h4 style='text-align: center; color: white;'>AI-powered insights into stock market trends.</h4>
+    <h4 style='text-align: center; color: white;'>Know Who’s Leaving—Before They Do.</h4>
 """, unsafe_allow_html=True)
 
 # --- Load model and objects with caching ---
